8a9259d60283db130a3246d53a02d3a976af27eb
RJ Bergeron <rbergeron@palantir.com>
2018-04-26 01:26:23 -0400

    clean out unneeded files for xen install

--- add-xen.sh
+++ add-xen.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/bash
 
-docker run -i --name newfs docker.io/redjays/xenial:xenhv /bin/true
+docker run -i --name newfs docker.io/redjays/xenial:xenhv bash -exs << _EOF_
+export TERM=dumb
+
+rm -f /etc/fstab
+rm -f /etc/mdadm/mdadm.conf
+rm -f /etc/keys/*
+rm -f /etc/crypttab
+_EOF_
 docker export newfs > xenhv.tar
 docker rm newfs
 pxz xenhv.tar
 
+mkdir -p images
+mv xenhv.tar.xz images
+
 rm -f xenhv_inst.iso
 
-xorriso -indev boottest.iso -outdev xenhv_inst.iso -as mkisofs -isohybrid-mbr . -partition_cyl_align 0 -partition_hd_cyl 0 -partition_hd_cyl 0 -partition_sec_hd 0 -c '/boot/boot.cat' -b '/isolinux/isolinux.bin' -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e '/boot/efiboot.img' -no-emul-boot -boot-load-size 24576 -isohybrid-gpt-basdat -eltorito-alt-boot -e '/boot/macboot.img' -no-emul-boot -boot-load-size 60 -isohybrid-gpt-basdat -- -add xenhv.tar.xz
+xorriso -indev installercore.iso -outdev xenhv_inst.iso -as mkisofs -isohybrid-mbr . -partition_cyl_align 0 -partition_hd_cyl 0 -partition_hd_cyl 0 -partition_sec_hd 0 -c '/boot/boot.cat' -b '/isolinux/isolinux.bin' -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e '/boot/efiboot.img' -no-emul-boot -boot-load-size 24576 -isohybrid-gpt-basdat -eltorito-alt-boot -e '/boot/macboot.img' -no-emul-boot -boot-load-size 60 -isohybrid-gpt-basdat -- -add images/xenhv.tar.xz
