98a401e221f31c7188d97b3c20f54ec03c268390
RJ Bergeron <rbergeron@palantir.com>
2018-04-30 14:40:49 -0400

    use rock ridge paths not strict isofs ones.

--- vc.sh
+++ vc.sh
@@ -4,9 +4,9 @@ newfiles=$(mktemp -d)
 cd="$(pwd)/${1}"
 
 mkdir -p "${newfiles}/boot/grub"
-isoinfo -x '/BOOT/GRUB/GRUB.CFG;1' -i "${cd}" | sed -e 's/console=ttyS1,115200/console=tty0/' > "${newfiles}/boot/grub/grub.cfg"
+isoinfo -R -x '/boot/grub/grub.cfg' -i "${cd}" | sed -e 's/console=ttyS1,115200/console=tty0/' > "${newfiles}/boot/grub/grub.cfg"
 mkdir -p "${newfiles}/isolinux"
-isoinfo -x '/ISOLINUX/SYSLINUX.CFG;1' -i "${cd}" | sed -e 's/serial 1 115200//' -e 's/console=ttyS1,115200/console=tty0/' > "${newfiles}/isolinux/syslinux.cfg"
+isoinfo -R -x '/isolinux/syslinux.cfg' -i "${cd}" | sed -e 's/serial 1 115200//' -e 's/console=ttyS1,115200/console=tty0/' > "${newfiles}/isolinux/syslinux.cfg"
 
 cd "${newfiles}"
 xorriso -indev "${cd}" -outdev "${cd}" -as mkisofs -isohybrid-mbr . -partition_cyl_align 0 -partition_hd_cyl 0 -partition_hd_cyl 0 -partition_sec_hd 0 -c '/boot/boot.cat' -b '/isolinux/isolinux.bin' -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e '/boot/efiboot.img' -no-emul-boot -boot-load-size 24576 -isohybrid-gpt-basdat -eltorito-alt-boot -e '/boot/macboot.img' -no-emul-boot -boot-load-size 60 -isohybrid-gpt-basdat -- -add "boot/grub/grub.cfg" "isolinux/syslinux.cfg"
